9c675358265f8ee6e2445c458a1c26355f77e2f1
Rigel Kent Carbonel <luigel@users.noreply.github.com>
2020-11-08 02:52:34 +0000

    Apply fixes from StyleCI

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,16 @@
 
 namespace Luigel\LaravelPassportViews\Tests;
 
-use Orchestra\Testbench\TestCase;
 use Luigel\LaravelPassportViews\LaravelPassportViewsServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [LaravelPassportViewsServiceProvider::class];
     }
-    
+
     /** @test */
     public function true_is_true()
     {
